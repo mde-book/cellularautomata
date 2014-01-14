8773094f9c710b0fd5cdd3b92cc8a1546c35c93e
nazari <nazari@d42ea84f-d241-4699-a319-0a476f7f3380>
2014-01-14 15:57:30 +0000

    added initialization grammar

--- monticore/cellularautomata-lang/cellularautomata-lang-initialization/src/main/grammars/org/kermeta/language/cellularautomata/rules/CellularAutomataInitialization.mc
+++ monticore/cellularautomata-lang/cellularautomata-lang-initialization/src/main/grammars/org/kermeta/language/cellularautomata/rules/CellularAutomataInitialization.mc
@@ -2,13 +2,43 @@ package org.kermeta.language.cellularautomata.rules;
 
 version "0.0.1";
 
-grammar CellularAutomataInitialization {
+grammar CellularAutomataInitialization extends org.kermeta.language.cellularautomata.rules.CellularAutomataCore {
 
 
   /* ======================================================================= */
   /* =============================== GRAMMAR =============================== */
   /* ======================================================================= */
 
-  Conditional;
+  CellularAutomataInitialization =
+    (geometry:RegularGeometry)?
+    seedRules:Rule (seedRules:Rule)*
+  ;
+
+
+  Rule =
+    "where"  "initValue" (filter:GlobalPosition) "=" "{" evaluatedVal:Conditional "}"
+  ;
+
+  GlobalPosition =
+	"{" coordinateRanges:CoordinateRange ("x" (coordinateRanges:CoordinateRange)?)* "}"
+  ;
+
+  CoordinateRange =
+	"[" (lowerCoordinate:IntLiteral) "," (upperCoordinate:IntLiteral) "]"
+  ;
+
+  InitializationLiteralsExpression extends LiteralsExpression = BooleanLiteral;
+
+  PositionLiteral =
+    "positionOn" "[" dimensionIndex:IntLiteral
+  ;
+
+  RegularGeometry =
+	"regularGeometry" neighborsNumber:IntLiteral "{" dimensions:Dimension  ("x" dimensions:Dimension)* "}"
+  ;
+
+  Dimension =
+	(size:IntLiteral) /* | (isCircular? = '(' size=EInt ')')*/ // TODO
+  ;
 
 }
---
+++ monticore/cellularautomata-lang/cellularautomata-lang-initialization/src/test/java/org/kermeta/language/sample/cellularautomata/rules/CellularAutomataInitializationGrammarTest.java
@@ -0,0 +1,75 @@
+/* Copyright (c) 2012 RWTH Aachen. All rights reserved.
+ *
+ * http://www.se-rwth.de/ */
+package org.kermeta.language.sample.cellularautomata.rules;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import mc.antlr.RecognitionException;
+import mc.antlr.TokenStreamException;
+import mc.grammar.MCConcreteParser;
+
+import org.junit.Test;
+import org.kermeta.language.cellularautomata.rules._parser.CellularAutomataInitializationUnaryExpressionMCConcreteParser;
+
+import com.google.common.base.Supplier;
+
+/**
+ * Tests if CellularAutomataCore.mc is producing the desired models.
+ *
+ * @author (last commit) $Author$
+ * @version $Revision$, $Date: 2012-11-01 16:19:26 +0100 (Do, 01 Nov
+ *          2012) $
+ *
+ */
+public class CellularAutomataInitializationGrammarTest {
+
+  public static final Supplier<MCConcreteParser> parserSupplier = new Supplier<MCConcreteParser>() {
+    @Override
+    public MCConcreteParser get() {
+      return new CellularAutomataInitializationUnaryExpressionMCConcreteParser("Test");
+    }
+  };
+
+
+
+  /**
+   * Tests if the models are really unparsable on basis
+   * of the current Cellular Automata Core grammar.
+   *
+   * @throws IOException
+   * @throws RecognitionException
+   * @throws TokenStreamException
+   */
+  public void parseAllInalidTestModels() throws IOException, RecognitionException,
+      TokenStreamException {
+
+    GrammarTestParser parser = GrammarTestParser.newParser(parserSupplier);
+    parser.parseModels(new File("src/test/resources/syntactically_invalid/syntax"), ".core");
+
+    assertTrue(parser.printReport(), parser.failedParsingAllModelFiles());
+  }
+
+  /**
+   * Tests if the models are really parsable on basis
+   * of the current Cellular Automata Core grammar.
+   *
+   * @throws IOException
+   * @throws RecognitionException
+   * @throws TokenStreamException
+   */
+  @Test
+  public void parseAllValidTestModels() throws IOException, RecognitionException,
+      TokenStreamException {
+
+    GrammarTestParser parser = GrammarTestParser.newParser(parserSupplier);
+    parser.parseModels(new File("src/test/resources/syntactically_valid"), ".irule");
+
+    assertTrue(parser.printReport(), parser.wasSuccessful());
+
+  }
+
+}
