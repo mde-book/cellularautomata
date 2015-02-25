e7043c865ddcf6f6063a52f35993609598e325c5
dvojtise <dvojtise@d42ea84f-d241-4699-a319-0a476f7f3380>
2015-02-25 22:18:45 +0000

    some clean up in the visitor code

--- samples/cellularautomata/dynamic_semantic/fr.inria.diverse.k3.example.cellularautomata.visitorbasedsimulator/src/fr/inria/diverse/k3/example/cellularautomata/visitorbasedsimulator/SimulatorVisitor.java
+++ samples/cellularautomata/dynamic_semantic/fr.inria.diverse.k3.example.cellularautomata.visitorbasedsimulator/src/fr/inria/diverse/k3/example/cellularautomata/visitorbasedsimulator/SimulatorVisitor.java
@@ -14,7 +14,6 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 class SimulatorVisitor implements EvolEvaluationVisitor{
-
 	public SimulatorContext context;
 
 	public SimulatorVisitor(SimulatorContext context) {
@@ -29,7 +28,7 @@ class SimulatorVisitor implements EvolEvaluationVisitor{
 			context.currentCell = cell;
 			Optional<Rule> rule = automata.getRules().stream().filter(r -> isRuleApplicableForCell(r, cell)).findFirst();
 			if(!rule.isPresent()){
-				// no rule, report the current value as the future
+				// no rule applicable, report the current value as the future
 				futureCellValues.put(cell, cell.getVal());
 			} else {
 				futureCellValues.put(cell, rule.get().accept(this));
@@ -48,7 +47,143 @@ class SimulatorVisitor implements EvolEvaluationVisitor{
 	public int visitIntegerLiteral(IntegerLiteral expression) {
 		return expression.getVal();
 	}
-	
+	@Override
+	public int visitAdd(Add expression) {
+		return expression.getLeft().accept(this) + expression.getRight().accept(this);
+	}
+	@Override
+	public int visitConditional(Conditional expression) {
+		if (expression.getCondition().accept(this) != 0) {
+			return expression.getIfTrueExpression().accept(this);
+		} else {
+			return expression.getIfFalseExpression().accept(this);				
+		}
+	}
+	@Override
+	public int visitEqual(Equal expression) {
+		int left = expression.getLeft().accept(this);
+		int right = expression.getRight().accept(this);
+		return left == right ? 1 : 0;
+	}
+	@Override
+	public int visitDiv(Div expression) {
+		return expression.getLeft().accept(this) / expression.getRight().accept(this);
+	}
+	@Override
+	public int visitGreater(Greater expression) {
+		int left = expression.getLeft().accept(this);
+		int right = expression.getRight().accept(this);
+		return left > right ? 1 : 0;
+	}
+	@Override
+	public int visitLower(Lower expression) {
+		int left = expression.getLeft().accept(this);
+		int right = expression.getRight().accept(this);
+		return left < right ? 1 : 0;
+	}
+	@Override
+	public int visitMinus(Minus expression) {
+		return expression.getLeft().accept(this) - expression.getRight().accept(this);
+	}
+	@Override
+	public int visitMod(Mod expression) {
+		return expression.getLeft().accept(this) % expression.getRight().accept(this);
+	}
+	@Override
+	public int visitMult(Mult expression) {
+		return expression.getLeft().accept(this) * expression.getRight().accept(this);
+	}
+	@Override
+	public int visitNot(Not expression) {
+		boolean target = expression.getTarget().accept(this) != 0 ;
+		return (!target) ? 1 : 0;
+	}
+	@Override
+	public int visitAnd(And expression) {
+		boolean left = expression.getLeft().accept(this) != 0 ;
+		boolean right = expression.getRight().accept(this) != 0;
+		return left && right ? 1 : 0;
+	}
+	@Override
+	public int visitOr(Or expression) {
+		boolean left = expression.getLeft().accept(this) != 0 ;
+		boolean right = expression.getRight().accept(this) != 0;
+		return left || right ? 1 : 0;
+	}
+	@Override
+	public int visitUMinus(UMinus expression) {
+		return -(expression.getTarget().accept(this));
+	}
+	@Override
+	public int visitCurrentCellPopulation(CurrentCellPopulation expression) {
+		return context.currentCell.getVal();
+	}
+	@Override
+	public int visitMax(Max expression) {
+		Stream<Integer> selectedCellValues;
+		if (expression.getNeighborsFilter() == null) {
+			selectedCellValues = context.currentCell.getNeighbors().stream().map(cellValue -> cellValue.getVal());
+		} else {
+			Filter filter = expression.getNeighborsFilter();
+			selectedCellValues = context.currentCell.getNeighbors().stream()
+					.filter(cell -> isFilterApplicableForCell(filter, cell))
+					.map(cellValue -> cellValue.getVal());
+		}
+		return selectedCellValues.reduce(Integer.MIN_VALUE, (p1, p2) -> p1 < p2 ? p2 : p1); 
+	}
+	@Override
+	public int visitMin(Min expression) {
+		Stream<Integer> selectedCellValues;
+		if (expression.getNeighborsFilter() == null) {
+			selectedCellValues = context.currentCell.getNeighbors().stream().map(cellValue -> cellValue.getVal());
+		} else {
+			Filter filter = expression.getNeighborsFilter();
+			selectedCellValues = context.currentCell.getNeighbors().stream()
+					.filter(cell -> isFilterApplicableForCell(filter, cell))
+					.map(cellValue -> cellValue.getVal());
+		}
+		return selectedCellValues.reduce(Integer.MAX_VALUE, (p1, p2) -> p1 > p2 ? p2 : p1);
+	}
+	@Override
+	public int visitSize(Size expression) {
+		if (expression.getNeighborsFilter() == null) {
+			return context.currentCell.getNeighbors().size();
+		} else {
+			Filter filter = expression.getNeighborsFilter();
+			return context.currentCell.getNeighbors().stream()
+					.filter(cell -> isFilterApplicableForCell(filter, cell))
+					.mapToInt(c -> 1)
+					.sum();
+		}
+	}
+	@Override
+	public int visitSum(Sum expression) {
+		if (expression.getNeighborsFilter() == null) {
+			return context.currentCell.getNeighbors().stream()
+					.mapToInt(cell -> cell.getVal()).sum();
+		} else {
+			Filter filter = expression.getNeighborsFilter();
+			return context.currentCell.getNeighbors().stream()
+					.filter(cell -> isFilterApplicableForCell(filter, cell))
+					.mapToInt(cell -> cell.getVal())
+					.sum();
+		}
+		
+	}
+
+	@Override
+	public int visitFilter(Filter filter) {
+		// do nothing, should never append
+		return 0;
+	}
+	@Override
+	public int visitPopulationRange(PopulationRange popRange) {
+		// do nothing, should never append
+		return 0;
+	}
+	//----------------
+	// visit methods related to abstract concepts
+	//----------------
 	@Override
 	public int visitIntegerExpression(IntegerExpression expression) {
 		// do nothing, should never append
@@ -67,169 +202,12 @@ class SimulatorVisitor implements EvolEvaluationVisitor{
 		return 0;
 		
 	}
-
-
-	@Override
-	public int visitAdd(Add expression) {
-		return expression.getLeft().accept(this) + expression.getRight().accept(this);
-	}
-
-	@Override
-	public int visitConditional(Conditional expression) {
-		if (expression.getCondition().accept(this) != 0) {
-			return expression.getIfTrueExpression().accept(this);
-		} else {
-			return expression.getIfFalseExpression().accept(this);				
-		}
-	}
-
-	@Override
-	public int visitDiv(Div expression) {
-		return expression.getLeft().accept(this) / expression.getRight().accept(this);
-	}
-
-	@Override
-	public int visitEqual(Equal expression) {
-		int left = expression.getLeft().accept(this);
-		int right = expression.getRight().accept(this);
-		return left == right ? 1 : 0;
-	}
-
-	@Override
-	public int visitFilter(Filter filter) {
-		// do nothing, should never append
-		return 0;
-	}
-
-	@Override
-	public int visitGreater(Greater expression) {
-		int left = expression.getLeft().accept(this);
-		int right = expression.getRight().accept(this);
-		return left > right ? 1 : 0;
-	}
-
-
-	@Override
-	public int visitLower(Lower expression) {
-		int left = expression.getLeft().accept(this);
-		int right = expression.getRight().accept(this);
-		return left < right ? 1 : 0;
-	}
-
-	@Override
-	public int visitMinus(Minus expression) {
-		return expression.getLeft().accept(this) - expression.getRight().accept(this);
-	}
-
-	@Override
-	public int visitMod(Mod expression) {
-		return expression.getLeft().accept(this) % expression.getRight().accept(this);
-	}
-
-	@Override
-	public int visitMult(Mult expression) {
-		return expression.getLeft().accept(this) * expression.getRight().accept(this);
-	}
-
-	@Override
-	public int visitNot(Not expression) {
-		boolean target = expression.getTarget().accept(this) != 0 ;
-		return (!target) ? 1 : 0;
-	}
-
-	@Override
-	public int visitAnd(And expression) {
-		boolean left = expression.getLeft().accept(this) != 0 ;
-		boolean right = expression.getRight().accept(this) != 0;
-		return left && right ? 1 : 0;
-	}
-	
-	@Override
-	public int visitOr(Or expression) {
-		boolean left = expression.getLeft().accept(this) != 0 ;
-		boolean right = expression.getRight().accept(this) != 0;
-		return left || right ? 1 : 0;
-	}
-
-	@Override
-	public int visitUMinus(UMinus expression) {
-		return -(expression.getTarget().accept(this));
-	}
-
-
-
-	@Override
-	public int visitCurrentCellPopulation(CurrentCellPopulation expression) {
-		return context.currentCell.getVal();
-	}
-
-	@Override
-	public int visitMax(Max expression) {
-		Stream<Integer> selectedCellValues;
-		if (expression.getNeighborsFilter() == null) {
-			selectedCellValues = context.currentCell.getNeighbors().stream().map(cellValue -> cellValue.getVal());
-		} else {
-			Filter filter = expression.getNeighborsFilter();
-			selectedCellValues = context.currentCell.getNeighbors().stream()
-					.filter(cell -> isFilterApplicableForCell(filter, cell))
-					.map(cellValue -> cellValue.getVal());
-		}
-		return selectedCellValues.reduce(Integer.MIN_VALUE, (p1, p2) -> p1 < p2 ? p2 : p1); 
-	}
-
-	@Override
-	public int visitMin(Min expression) {
-		Stream<Integer> selectedCellValues;
-		if (expression.getNeighborsFilter() == null) {
-			selectedCellValues = context.currentCell.getNeighbors().stream().map(cellValue -> cellValue.getVal());
-		} else {
-			Filter filter = expression.getNeighborsFilter();
-			selectedCellValues = context.currentCell.getNeighbors().stream()
-					.filter(cell -> isFilterApplicableForCell(filter, cell))
-					.map(cellValue -> cellValue.getVal());
-		}
-		return selectedCellValues.reduce(Integer.MAX_VALUE, (p1, p2) -> p1 > p2 ? p2 : p1);
-	}
-
 	@Override
 	public int visitNeighborsExpression(NeighborsExpression expression) {
 		// do nothing, should never append
 		return 0;
 	}
 
-	@Override
-	public int visitPopulationRange(PopulationRange popRange) {
-		// do nothing, should never append
-		return 0;
-	}
-
-	@Override
-	public int visitSize(Size expression) {
-		if (expression.getNeighborsFilter() == null) {
-			return context.currentCell.getNeighbors().size();
-		} else {
-			Filter filter = expression.getNeighborsFilter();
-			return context.currentCell.getNeighbors().stream()
-					.filter(cell -> isFilterApplicableForCell(filter, cell))
-					.mapToInt(c -> 1)
-					.sum();
-		}
-	}
-
-	@Override
-	public int visitSum(Sum expression) {
-		if (expression.getNeighborsFilter() == null) {
-			return context.currentCell.getNeighbors().stream()
-					.mapToInt(cell -> cell.getVal()).sum();
-		} else {
-			Filter filter = expression.getNeighborsFilter();
-			return context.currentCell.getNeighbors().stream()
-					.filter(cell -> isFilterApplicableForCell(filter, cell))
-					.mapToInt(cell -> cell.getVal())
-					.sum();
-		}
-		
-	}
 
 	//----------------
 	// Helper methods
