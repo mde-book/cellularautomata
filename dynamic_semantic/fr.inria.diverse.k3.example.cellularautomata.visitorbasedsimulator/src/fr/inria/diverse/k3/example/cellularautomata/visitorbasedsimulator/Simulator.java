5d2754728230f608ce8289b29910a77b30849c6e
dvojtise <didier.vojtisek@inria.fr>
2016-04-01 14:52:16 +0000

    improved some listing presentation in chapter 7 added description of [..] usage in listings

--- dynamic_semantic/fr.inria.diverse.k3.example.cellularautomata.visitorbasedsimulator/src/fr/inria/diverse/k3/example/cellularautomata/visitorbasedsimulator/Simulator.java
+++ dynamic_semantic/fr.inria.diverse.k3.example.cellularautomata.visitorbasedsimulator/src/fr/inria/diverse/k3/example/cellularautomata/visitorbasedsimulator/Simulator.java
@@ -1,18 +1,19 @@
+// MDE_BOOK_START
 package fr.inria.diverse.k3.example.cellularautomata.visitorbasedsimulator;
-
+// MDE_BOOK_END
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+// MDE_BOOK_START
 import evol.CellularAutomata;
 import evol.EvolPackage;
 import vm.Universe;
 import vm.VmPackage;
 
 public class Simulator {
-
 	public void simulateWithAsciiArt2Dvisualizer(String ruleFile, String gridFile, String universeSize) { 
 		Universe grid = loadGrid(gridFile);
 		CellularAutomata automata = loadRule(ruleFile);
@@ -43,6 +44,7 @@ public class Simulator {
 	}
 	
 	public  Universe loadGrid(String gridFile) {
+// MDE_BOOK_END
 		 XMIResourceFactoryImpl fact = new XMIResourceFactoryImpl();
 		if (!EPackage.Registry.INSTANCE.containsKey(VmPackage.eNS_URI)) {
 			EPackage.Registry.INSTANCE.put(VmPackage.eNS_URI, VmPackage.eINSTANCE);
@@ -50,8 +52,10 @@ public class Simulator {
 		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("*", fact);
 	
 		ResourceSet rs = new ResourceSetImpl();
+// MDE_BOOK_START
 		URI uri = URI.createURI(gridFile);
 		Resource res = rs.getResource(uri, true);
 		return (Universe)(res.getContents().get(0));
 	}
 }
+// MDE_BOOK_END
