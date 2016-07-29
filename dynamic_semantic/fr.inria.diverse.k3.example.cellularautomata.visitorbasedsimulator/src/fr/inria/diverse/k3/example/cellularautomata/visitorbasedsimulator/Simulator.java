5c36f2427375cbb2ae51a737fbd859d13d75be9d
dvojtise <didier.vojtisek@inria.fr>
2016-07-29 22:42:14 +0000

    trimming liting to fit the pages

--- dynamic_semantic/fr.inria.diverse.k3.example.cellularautomata.visitorbasedsimulator/src/fr/inria/diverse/k3/example/cellularautomata/visitorbasedsimulator/Simulator.java
+++ dynamic_semantic/fr.inria.diverse.k3.example.cellularautomata.visitorbasedsimulator/src/fr/inria/diverse/k3/example/cellularautomata/visitorbasedsimulator/Simulator.java
@@ -1,18 +1,18 @@
-// MDE_BOOK_START
 package fr.inria.diverse.k3.example.cellularautomata.visitorbasedsimulator;
-// MDE_BOOK_END
+
+// MDE_BOOK_START
+import evol.CellularAutomata;
+import evol.EvolPackage;
+import vm.Universe;
+import vm.VmPackage;
+//MDE_BOOK_END
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-// MDE_BOOK_START
-import evol.CellularAutomata;
-import evol.EvolPackage;
-import vm.Universe;
-import vm.VmPackage;
-
+//MDE_BOOK_START
 public class Simulator {
 	public void simulateWithAsciiArt2Dvisualizer(String ruleFile, String gridFile, String universeSize) { 
 		Universe grid = loadGrid(gridFile);
@@ -20,7 +20,7 @@ public class Simulator {
 		Integer universeLength = Integer.parseInt(universeSize);
 		
 		SimpleAsciiArt2DVisualizer asciiArtVisualizer = new SimpleAsciiArt2DVisualizer();
-		asciiArtVisualizer.visualizeRegular2DUniverse(universeLength, grid);
+		asciiArtVisualizer.visualizeRegular2DUniverse(universeLength,grid);
 		
 		SimulatorContext context = new SimulatorContext(grid);
 		SimulatorVisitor simVisitor = new SimulatorVisitor(context);
@@ -28,15 +28,13 @@ public class Simulator {
 			automata.accept(simVisitor);
 			asciiArtVisualizer.visualizeRegular2DUniverse(universeLength, grid);
 		}
-	}
-		
+	}		
 	public CellularAutomata loadRule(String ruleFile) {
 		XMIResourceFactoryImpl fact = new XMIResourceFactoryImpl();
 		if (!EPackage.Registry.INSTANCE.containsKey(EvolPackage.eNS_URI)) {
 			EPackage.Registry.INSTANCE.put(EvolPackage.eNS_URI, EvolPackage.eINSTANCE);
 		}
 		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("*", fact);
-	
 		ResourceSet rs = new ResourceSetImpl();
 		URI uri = URI.createURI(ruleFile);
 		Resource res = rs.getResource(uri, true);
